76b0cc9f298e1effc97179425e1b649318c102af
front-boombox <glubokij85@gmail.com>
2020-03-18 22:57:52 +0200

    sdf

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,29 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post, PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
 
+  public post: Post;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private postsService: PostsService,
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.post = this.postsService.getById(+params.id);
+    });
+  }
+
+  public loadPost() {
+    this.router.navigate(['/posts', 44]);
+  }
 }
